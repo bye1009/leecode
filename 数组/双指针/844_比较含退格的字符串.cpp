ef39e40909befddf93a26fb7024f1aec7d4bf8d9
bye1009 <1275817506@qq.com>
2023-07-02 16:46:52 +0800

    修改844

--- 数组/双指针/844_比较含退格的字符串.cpp
+++ 数组/双指针/844_比较含退格的字符串.cpp
@@ -4,37 +4,50 @@ using namespace std;
 
 class Solution {
     public:
-        void getBackspaceStr(string s){
-            int ns = s.size();
-            int j = 0;
-            for(int i = 0;i < ns; i++){
-                if (s[i] != '#'){
-                    s[j] = s[i];
+        int getBackspaceStr(string& str){
+            cout<<str<<endl;
+            int n = str.size();
+            int i=0, j = 0;
+            cout<<"i:"<<i<<" j:"<<j<<endl;
+            while (i<n){
+                if(str[i]!='#'){
+                    str[j] = str[i];
+                    i++;
                     j++;
                 }
+                //Óöµ½#¾ÍÍË
                 else{
                     j--;
+                    if (j<0)
+                        j = 0;
                     i++;
-                    if(j>0 && i<ns){
-                        s[j] = s[i];
-                    }
                 }
             }
+            cout<<j<<endl;
+            for(int k=0;k<j;k++)
+                cout<<str[k];
+            cout<<endl;
+            return j;
         }
         bool backspaceCompare(string s, string t) {
-            getBackspaceStr(s);
-            getBackspaceStr(t);
-            if (s==t)
-                return 'true';
-            else
-                return 'false';
+            int ns = getBackspaceStr(s);
+            int nt = getBackspaceStr(t);
+            if(ns!=nt)
+                return false;
+            else{
+                for(int i=0;i<ns;i++){
+                    if (s[i]!=t[i])
+                        return false;
+                }
+            }
+            return true;
         }
 };
 
 
 int main(){
-    string s = "ab#c";
-    string t = "ad#c";
+    string s = "addd#c";
+    string t = "ab#c";
     Solution sl;
     bool a = sl.backspaceCompare(s,t);
     cout << a;
